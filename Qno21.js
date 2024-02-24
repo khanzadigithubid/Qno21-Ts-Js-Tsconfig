56d7a8aab47df9a02985950ad5d0ba0d9194894b
Khanzadi Wazir Ali <160390882+khanzadigithubid@users.noreply.github.com>
2024-02-24 13:55:40 +0500

    Qno21 Ts/Js/Tsconfig and node js of 45 Assignments
    Qno21 Ts/Js/Tsconfig and node js of 45 Assignments

---
+++ Qno21.js
@@ -0,0 +1,11 @@
+"use strict";
+/*21. They think of something you could store in a TypeScript
+ Object. Write a program that creates Objects containing these
+ items.*/
+let myLaptop = {
+    brand: "Dell",
+    model: "XPS 13",
+    screenSize: 13.4,
+    isTouchscreen: true,
+};
+console.log("MyLaptop:", myLaptop);
---
+++ Qno21.ts
@@ -0,0 +1,21 @@
+/*21. They think of something you could store in a TypeScript
+ Object. Write a program that creates Objects containing these 
+ items.*/
+
+let myLaptop: {
+    brand: string;
+    model: string;
+    screenSize: number;
+    isTouchscreen: boolean;
+  } = {
+     brand: "Dell",
+     model: "XPS 13",
+     screenSize: 13.4,
+     isTouchscreen: true,
+  }
+
+  console.log("MyLaptop:", myLaptop);
+  
+  
+
+
